ac188c85f5fe1cfe07ef16161713c26d2a135711
Ahmed <Ahmed_Bahajjaj@u.nus.edu>
2019-01-19 23:30:09 +0800

    Added firebase initialisation and code structure

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
+import firebase from "firebase";
 import './App.css';
+
+var config = {
+  apiKey: "AIzaSyAb6W6yLDpM_E7qPgh3lOmw6iqERysHUDg",
+  authDomain: "usc-website-206715.firebaseapp.com",
+  databaseURL: "https://usc-website-206715.firebaseio.com",
+  projectId: "usc-website-206715",
+  storageBucket: "usc-website-206715.appspot.com",
+  messagingSenderId: "115895791273"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        {/*
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        */}
           <h1>
             Cinnamon College Laundry Tracker
           </h1>
+          <h2>
+            Floor 17
+          </h2>
+          {
+            //Insert Floor (Table) here
+          }
         </header>
       </div>
     );
   }
+}
+
+class Floor extends Component {
+  //Table
+}
+
+class Machine extends Component {
+  //Square cell of table
 }
 
 export default App;
