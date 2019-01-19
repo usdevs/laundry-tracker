1c7b849767ad3ef1e0edbb135dde8660c53709c6
Yuchuan <yyc478@gmail.com>
2019-01-20 00:19:36 +0800

    Updates

---
+++ precache-manifest.13bee5725a9396b649b25956d56311ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc440f1a0983b94d6160",
+    "url": "/laundry-tracker/static/js/runtime~main.dc440f1a.js"
+  },
+  {
+    "revision": "efd6e324c652a1e3e841",
+    "url": "/laundry-tracker/static/js/main.efd6e324.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/laundry-tracker/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "efd6e324c652a1e3e841",
+    "url": "/laundry-tracker/static/css/main.8ed5db09.chunk.css"
+  },
+  {
+    "revision": "ebaf4b2641f4e3c9b783343a98b81e43",
+    "url": "/laundry-tracker/index.html"
+  }
+];
