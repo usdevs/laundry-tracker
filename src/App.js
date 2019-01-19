f24b1a6e8483f32b603fc8919817a09117f28e51
Ahmed <Ahmed_Bahajjaj@u.nus.edu>
2019-01-19 21:49:03 +0800

    Added firebase to dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        {/*
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +20,10 @@ class App extends Component {
           >
             Learn React
           </a>
+        */}
+          <h1>
+            Cinnamon College Laundry Tracker
+          </h1>
         </header>
       </div>
     );
